837201ab4aef6c879bd1e1b644af41eb12c7a7b9
harika <harika.com>
2024-12-17 12:44:40 +0530

    updated repository layer

--- src/main/java/com/example/demo/repo/CoursePlanRepo.java
+++ src/main/java/com/example/demo/repo/CoursePlanRepo.java
@@ -1,9 +1,14 @@
 package com.example.demo.repo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.model.Batch;
 import com.example.demo.model.CoursePlan;
 
 public interface CoursePlanRepo extends JpaRepository<CoursePlan, Integer>{
 
+	public List<CoursePlan> findByBatch(Batch batch);
 }
